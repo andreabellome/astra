c036718d24bb4571972609c691a4a39f111a6d91
andreabellome <andrea.bellome@gmail.com>
2024-09-03 12:05:03 +0200

    upd astra dates

--- ASTRA/Dynamic programming/ASTRA_dates.m
+++ ASTRA/Dynamic programming/ASTRA_dates.m
@@ -1,7 +1,21 @@
 function [OUTPUT] = ASTRA_dates(INPUT, seq)
 
-% --> use SODP over a span of launch dates, i.e., find minimum for each
-% date
+% DESCRIPTION
+% This function uses SODP (Single Objective Direct Problem) to find the
+% optimal trajectory for a spacecraft given a range of launch dates.
+% It iteratively computes the trajectory and cost for each launch date,
+% then post-processes the results to determine the minimum cost trajectory.
+% 
+% INPUT
+% - INPUT : structure containing mission parameters, including launch date
+%           options, plotting options, and other necessary configurations.
+% - seq   : sequence of IDs for flyby bodies (see constants.m)
+% 
+% OUTPUT
+% - OUTPUT : structure array containing the computed trajectories and their
+%            associated costs for each considered launch date.
+% 
+% -------------------------------------------------------------------------
 
 % --> SODP 
 TT0        = [INPUT.depOpts(1):INPUT.depOpts(3):INPUT.depOpts(2)]'; % --> launch date vector
