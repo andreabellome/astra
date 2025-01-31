1758bbd850f66f447667926e7c484e58e9b4907e
andreabellome <andrea.bellome@gmail.com>
2025-01-31 19:15:57 +0100

    Update ASTRA_wrapPath_DP.m

--- ASTRA/Dynamic programming/ASTRA_wrapPath_DP.m
+++ ASTRA/Dynamic programming/ASTRA_wrapPath_DP.m
@@ -113,7 +113,7 @@ path(1,15)    = path(end,9);                               % --> arrival vinf (k
 path(1,16)    = sum(path(2:end,11))/365.25;                % --> TOF (years)
 
 if nargout > 1 % --> if requested from the user, plot the trajectory
-    fig = plotPath(path, idcentral);
+    fig = plotPath(path, idcentral, customEphemerides);
 end
 
 end
