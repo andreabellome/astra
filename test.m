2e571dc733e4d5920cfd32e933c31c04d1b4a874
Andrea Bellome <67928580+andreabellome@users.noreply.github.com>
2025-03-03 12:28:11 +0100

    Update test.m

--- test.m
+++ test.m
@@ -1,5 +1,14 @@
 
+[muCentral, mu_earth] = constants(1, 3)
 
+rpip = 500 + 6378;
+rat  = 500 + 6378;
+
+sma = 0.5*( rpip + rat )
+ecc = ( rat - rpip )/( rpip + rat )
+period = 2*pi*sqrt( sma*sma*sma/mu_earth )/86400
+
+[ dv ] = deltaV_hyperbola( 0.65, rpip, rat, mu_earth );
 
 [mf, k, mp] = rocketEq_givenDV(2e3, 1.943e3, 330);
 mp
