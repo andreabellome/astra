1121038c50c7e8a9155a8c497ca1e88544cca774
andreabellome <andrea.bellome@gmail.com>
2025-04-09 19:06:52 +0200

    rm things

--- improvedOrbitalIndicator.m
+++
@@ -1,8 +0,0 @@
-function xx = improvedOrbitalIndicator(rrs, vvs, rrt, vvt, DT)
-
-% --> rr (km), vv (km/s), DT (sec)
-
-% --> improved orbital indicator
-xx = [ (1./DT).*rrs + vvs , (1./DT).*rrs, (1./DT).*rrt - vvt, (1./DT).*rrt ];
-
-end
--- orbitalIndicator.m
+++
@@ -1,8 +0,0 @@
-function [xx] = orbitalIndicator(rr, vv, DT)
-
-% --> rr (km), vv (km/s), DT (sec)
-
-% --> orbital indicator
-xx = [(1./DT).*rr + vv , (1./DT).*rr];
-
-end
