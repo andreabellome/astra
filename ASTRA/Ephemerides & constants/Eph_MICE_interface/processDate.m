d5fc12551d5faaf1d39c78edc07ab7f9824a06f4
andreabellome <andrea.bellome@gmail.com>
2025-04-09 19:24:11 +0200

    Update processDate.m

--- ASTRA/Ephemerides & constants/Eph_MICE_interface/processDate.m
+++ ASTRA/Ephemerides & constants/Eph_MICE_interface/processDate.m
@@ -1,6 +1,20 @@
 function [date] = processDate(t)
 
-% --> t is in mjd2000
+% DESCRIPTION
+% This function converts an input time expressed in Modified Julian Date
+% 2000 (MJD2000) into a formatted date string compatible with the NASA
+% NAIF MICE Toolkit. It is primarily used for trajectory design problems
+% involving multiple gravity assists.
+%
+% INPUT
+% - t    : time in Modified Julian Date 2000 (MJD2000)
+%
+% OUTPUT
+% - date : formatted date string 'YYYY MM DD HH.mmssssssss' (TDB time scale)
+%          to be passed to SPICE functions such as cspice_str2et
+%
+% -------------------------------------------------------------------------
+
 
 date = mjd20002date(t);
 
