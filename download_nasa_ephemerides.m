fc6a607252b2e1b18c9339fda0bc91a7a5617c8f
andreabellome <andrea.bellome@gmail.com>
2025-04-10 20:29:26 +0200

    upd download nasa ephemerides

---
+++ download_nasa_ephemerides.m
@@ -0,0 +1,40 @@
+
+clearDeleteAdd; % --> !!! ONLY CALL IT ONCE FOR SPEED
+
+% --> load mice
+MICE_path = './MICE_TOOLBOX';
+addpath(genpath(MICE_path)); % --> always include this
+cspice_furnsh([MICE_path '/data.mk']);
+
+%%
+
+% --> central ID (Sun in this case)
+idcentral = 1;
+
+% --> lower and upper bounds for ephemerides
+t0 = [ 2030 1 1 12 0 0 ];
+tf = [ 2100 1 1 12 0 0 ];
+
+% --> SPKID of the object (found at https://ssd.jpl.nasa.gov/tools/sbdb_lookup.html#/)
+spk_id = 1000508;
+
+% --> set the path where to save the ephemerides
+spk_dir = pwd;
+
+% --> run the code (if success=1 then everything is okay)
+success = getSPK(num2str(spk_id), num2str(t0), num2str(tf), spk_dir, 'overwrite', 'on');
+
+%% --> check that you can use the ephemerides properly
+
+% --> load custom ephemerides
+MICE_path = './MICE_TOOLBOX';
+addpath(genpath(MICE_path)); % --> always include this
+cspice_furnsh([MICE_path '/data.mk']);
+
+cspice_furnsh([ spk_dir '\' num2str(spk_id) '.bsp']); % --> load the object ephemerides
+
+INPUT.customEphemerides = @EphSS_from_mice;
+
+t0_mjd2000 = date2mjd2000(t0);
+[rr, vv]   = INPUT.customEphemerides( spk_id, t0_mjd2000, 1 );
+
