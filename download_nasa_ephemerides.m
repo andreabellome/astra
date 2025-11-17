3686bea2ab8cd2fc1bdde9500750cdd21f11a879
Andrea Bellome <67928580+andreabellome@users.noreply.github.com>
2025-11-17 11:10:35 +0100

    Update download_nasa_ephemerides.m

--- download_nasa_ephemerides.m
+++ download_nasa_ephemerides.m
@@ -17,7 +17,7 @@ t0 = [ 2030 1 1 12 0 0 ];
 tf = [ 2100 1 1 12 0 0 ];
 
 % --> SPKID of the object (found at https://ssd.jpl.nasa.gov/tools/sbdb_lookup.html#/)
-spk_id = 1000508;
+spk_id = 20000001; % --> Ceres
 % spk_id = 20000021; % --> Lutetia
 % spk_id = 20150591;
 % spk_id = 20348435; 
