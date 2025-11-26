efcbc50fb0d2758cdb47979805be037e782f598e
Andrea Bellome <67928580+andreabellome@users.noreply.github.com>
2025-11-26 10:46:24 +0100

    Update download_nasa_ephemerides.m

--- download_nasa_ephemerides.m
+++ download_nasa_ephemerides.m
@@ -13,8 +13,8 @@ cspice_furnsh([MICE_path '/data.mk']);
 idcentral = 1;
 
 % --> lower and upper bounds for ephemerides
-t0 = [ 2030 1 1 12 0 0 ];
-tf = [ 2100 1 1 12 0 0 ];
+t0 = [ 2000 1 1 12 0 0 ];
+tf = [ 2050 1 1 12 0 0 ];
 
 % --> SPKID of the object (found at https://ssd.jpl.nasa.gov/tools/sbdb_lookup.html#/)
 spk_id = 20000001; % --> Ceres
