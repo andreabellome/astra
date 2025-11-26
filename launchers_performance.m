d22c97abfa563b6ce26d78a612fb7dbaa51d5322
Andrea Bellome <67928580+andreabellome@users.noreply.github.com>
2025-11-26 10:48:23 +0100

    Update launchers_performance.m

--- launchers_performance.m
+++ launchers_performance.m
@@ -4,8 +4,8 @@ close all; clc;
 
 %% --> Soyuz ST-B & Ariane 62
 
-vinf         = 1:1:6;
-mass_adapter = 0;
+vinf         = 1:1:6;   % --> dep. infinity velocity [km/s]
+mass_adapter = 0;       % --> launcher adapter mass [kg]
 
 mass_soyuz_stb = soyuz_stb_launcher( vinf, mass_adapter );
 mass_ariane_62 = ariane_62_launcher( vinf, mass_adapter );
@@ -20,10 +20,10 @@ plotFontSizeAxesDim(14, 14, fig, gca);
 
 %% --> Ariane 64 -- mono-boost performances
 
-vinf         = 1:1:6;
-dla_vec_deg  = [-5 0 5];
-mass_adapter = 0;
-type         = 'mono_boost';
+vinf         = 1:1:6;        % --> dep. infinity velocity [km/s]
+dla_vec_deg  = [-5 0 5];     % --> declination at launch [deg]
+mass_adapter = 0;            % --> launcher adapter mass [kg]
+type         = 'mono_boost'; % --> type (either 'mono_boost' or 'bi_boost')
 
 fig = figure( 'Color', [1 1 1] );
 hold on; grid on;
@@ -41,10 +41,10 @@ plotFontSizeAxesDim(14, 14, fig, gca);
 
 %% --> Ariane 64 -- bi-boost performances
 
-vinf         = 1:1:6;
-dla_vec_deg  = [-40 -30 -20 -10 0 10 20 30 40];
-mass_adapter = 0;
-type         = 'bi_boost';
+vinf         = 1:1:6;                           % --> dep. infinity velocity [km/s]
+dla_vec_deg  = [-40 -30 -20 -10 0 10 20 30 40]; % --> declination at launch [deg]
+mass_adapter = 0;                               % --> launcher adapter mass [kg]
+type         = 'bi_boost';                      % --> type (either 'mono_boost' or 'bi_boost')
 
 colors = cool(length(dla_vec_deg));
 
